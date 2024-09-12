74f06e86529136a18b5bf8d3e7cdf48f7a9c56c1
uijin27 <tuffjck0827@naver.com>
2024-09-12 17:36:40 +0900

    Update hello_world.py
    ..

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,9 @@
 def print_hello():
     print("Hello World!!")
 
+def print_kit():
+    print("kumoh National institute of Technology.")
+
 if __name__ == '__main__':
+    print_KIT()
     print_hello()
